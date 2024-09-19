f361bc9953072296b5ceee1803931dfdd485eac5
SaikatAhmed78 <shaikatahmed78@gmail.com>
2024-09-20 03:56:35 +0600

    Added money input and pin validation functionality for updating balance

--- js/location.js
+++ js/location.js
@@ -5,4 +5,20 @@ document.getElementById('add-money-btn').addEventListener('click', function(even
    
     const addMoneyInput = document.getElementById('add-money-input').value;
    const  pinNumberInput = document.getElementById('pin-number-input').value;
+
+   if(pinNumberInput === '2580'){
+  
+
+    const currentBalance = document.getElementById('current-balance').innerText;
+
+    const addMoneyNumber = parseFloat(addMoneyInput);
+    const balanceNumber = parseFloat(currentBalance);
+    const updateBalance = addMoneyNumber + balanceNumber;
+    console.log(updateBalance)
+
+    document.getElementById('current-balance').innerText =updateBalance;
+   }
+   else{
+    alert('Failed to add money! please try again')
+   }
 })

82c4722bc334687a8352be0fd5100bcc7610b32e
SaikatAhmed78 <shaikatahmed78@gmail.com>
2024-09-20 01:00:41 +0600

    added home page connection

--- js/login.js
+++ js/login.js
@@ -1,8 +1,31 @@
 
-document.getElementById('login-button').addEventListener('click', function(event){ 
+// document.getElementById('login-button').addEventListener('click', function(event){ 
+// event.preventDefault();
+
+// const phoneNumber = document.getElementById('phone-number').value;
+// const pinNumber = document.getElementById('pin-number').value;
+
+// if(phoneNumber === '5' && pinNumber  === 1234){
+//     console.log('you are logged in');
+// }
+// else{
+//     alert('check your email or phone')
+// }
+
+// })
+
+
+document.getElementById('login-button').addEventListener('click', function(event){
 event.preventDefault();
 
 const phoneNumber = document.getElementById('phone-number').value;
-console.log(phoneNumber)
+const pinNumber = document.getElementById('pin-number').value;
 
+if(phoneNumber === '017' && pinNumber === '2580'){
+    console.log('you are logged in');
+    window.location.href = '/location.html'
+}
+else{
+    alert('type your valid information')
+}
 })

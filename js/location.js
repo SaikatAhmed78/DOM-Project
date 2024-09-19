530299863d75f22e7f61872a4d332e701ecc0825
SaikatAhmed78 <shaikatahmed78@gmail.com>
2024-09-20 01:54:10 +0600

    added partially the add money event handler

---
+++ js/location.js
@@ -0,0 +1,8 @@
+//add money to the account
+
+document.getElementById('add-money-btn').addEventListener('click', function(event){
+    event.preventDefault();
+   
+    const addMoneyInput = document.getElementById('add-money-input').value;
+   const  pinNumberInput = document.getElementById('pin-number-input').value;
+})
